84c50fa11cdcc61ef6546dde2e151b9390774e94
ludovicferre <ludovic.ferre@gmail.com>
2015-05-15 14:11:03 +0200

    Adding a thread pool implementation to simplify code

--- pingcomputers.cs
+++ pingcomputers.cs
@@ -24,7 +24,7 @@ select distinct(s.[Host Name] + '.' + s.[Primary DNS Suffix])
   from vSiteServices s
  order by s.[Host Name] + '.' + s.[Primary DNS Suffix]
 */
--- set rowcount 1500
+set rowcount 500
 select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
   from Inv_Client_Task_Resources r
   join Inv_AeX_AC_TCPIP i
@@ -72,7 +72,7 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 
 				foreach (Thread t in pool) {
 					Console.Write(".");
-					t.Join();
+					t.Join(1000);
 				}
 
 				Console.WriteLine("\n\rDequeueing results (we have {0} entries)...", p.ResultQueue.Count.ToString());
@@ -80,19 +80,19 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 				// Move to stage 2: check the Altiris Agent status if possible
 				ServiceChecker sc = new ServiceChecker();
 
-				KeyValuePair<string, int> results = new KeyValuePair<string, int>();
+				KeyValuePair<string, string> results = new KeyValuePair<string, string>();
 				while (p.ResultQueue.Count > 0) {
-					results = (KeyValuePair<string, int>) p.ResultQueue.Dequeue();
-					if (results.Value != 0) {
-						Console.WriteLine("{1} for host {0}.", results.Key, (results.Value != 0) ? "SUCCESS" : "Failure");
+					results = (KeyValuePair<string, string>) p.ResultQueue.Dequeue();
+					if (results.Value == "1") {
 						sc.HostQueue.Enqueue(results.Key);
 					}
 				}
 								
 				// Reset the thread pool and limit
-				pool.Clear();
+//				pool.Clear();
 				pool_depth = sc.HostQueue.Count / 5;
-				
+
+/*				
 				// Make sure we don't have more than 50 threads
 				if (pool_depth > 50)
 					pool_depth = 50;
@@ -108,24 +108,25 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 					t.Start();
 					pool.Add(t);
 				}
-
+*/					
 				Thread n = new Thread(new ThreadStart(sc.PrintStatus));
 				n.Start();
+		
+				ThreadPool tp = new ThreadPool();
+				tp.PoolDepth = pool_depth;
 				
-				Console.Write("Waiting for threads to converg back...");
+				tp.StartAll(sc.RunCheck);
+				Console.WriteLine("Waiting for threads to converge back...");
+
 				n.Join();
-				
+				tp.JoinAll();
+
 				foreach (Thread t in pool) {
 					Console.Write("!");
-					t.Join();
+					t.Join(1000);
 				}
 				Console.WriteLine("\n\nDequeueing results (we have {0} entries)...", sc.ResultQueue.Count.ToString());
-				
-				KeyValuePair<string, string> sc_results = new KeyValuePair<string, string>();
-				while (sc.ResultQueue.Count > 0) {
-					sc_results = (KeyValuePair<string, string>) sc.ResultQueue.Dequeue();
-					Console.WriteLine("Altiris Agent service status for host {0}: {1}", sc_results.Key, sc_results.Value);
-				}
+				sc.PrintResults();
 
 			} catch (Exception e) {
 				// EventLog.ReportError(String.Format("{0}\n{1}", e.Message, e.InnerException));
@@ -133,7 +134,51 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 			}
             Timer.Stop();
 		}
+		
     }
+
+
+	class ThreadPool {
+		private int pool_depth;
+		public Collection<Thread> pool;
+				
+		public ThreadPool() {
+			pool_depth = 10;
+			pool = new Collection<Thread>();
+		}
+		
+		public int PoolDepth {
+			set {
+				if (value < 1)
+					pool_depth = 1;
+				else if (value > 50)
+					pool_depth = 50;
+				else
+					pool_depth = value;
+			}
+			get {
+				return pool_depth;
+			}
+		}
+		
+		public void StartAll(ThreadStart st) {
+			for (int i = 0; i < pool_depth; i++) {
+				Thread t = new Thread(st);
+				t.Start();
+				Add(t);
+			}
+		}
+		
+		private void Add(Thread t) {
+			pool.Add(t);
+		}
+		
+		public void JoinAll() {
+			foreach(Thread t in pool) {
+				t.Join(1000);
+			}
+		}
+	}
 	
 	class QueueWorker {
 		private Queue baseHostQueue;
@@ -147,7 +192,7 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 			init();
 		}
 		
-		public void init() {
+		public 	void init() {
 			ThreadPoolDepth = 0;
 			baseResultQueue = new Queue();
 			ResultQueue = Queue.Synchronized(baseResultQueue);
@@ -156,14 +201,22 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 			HostQueue = Queue.Synchronized(baseHostQueue);
 		}
 		
-		public virtual void PrintStatus() {}
-		public virtual void SaveResults() {}
-		public virtual void PrintResults() {}
+		public void PrintStatus() {
+			while (HostQueue.Count > 0) {
+				Console.Write("Currently running {0} threads, {1} queued hostnames, {2} tested hostnames.\r", ThreadPoolDepth.ToString(), HostQueue.Count.ToString(), ResultQueue.Count.ToString());
+				Thread.Sleep(1000);
+			}
+			Console.WriteLine("Currently running {0} threads, {1} queued hostnames, {2} tested hostnames.", ThreadPoolDepth.ToString(), HostQueue.Count.ToString(), ResultQueue.Count.ToString());
 
-		public void StoreResult(string hostname, int status) {
-			KeyValuePair<string, int> kvp = new KeyValuePair<string, int>(hostname, status);
-			ResultQueue.Enqueue(kvp);
 		}
+
+		public void PrintResults() {
+			foreach (KeyValuePair<string, string> kvp in ResultQueue) {
+				Console.WriteLine("Status = {1} for host {0}.", kvp.Key, kvp.Value);
+			}
+		}
+
+		public virtual void SaveResults() {}
 
 		public void StoreResult(string hostname, string status) {
 			KeyValuePair<string, string> kvp = new KeyValuePair<string, string>(hostname, status);
@@ -183,8 +236,7 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 		public void RunPing () {
 			string hostname = "";
 			while ((hostname = GetHostname()) != "")  {
-				// Do the ping
-				string tid =  Thread.CurrentThread.ManagedThreadId.ToString();
+				// string tid =  Thread.CurrentThread.ManagedThreadId.ToString();
 				try {
 					Ping ping = new Ping();
 					
@@ -192,33 +244,19 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 					PingReply result = ping.Send(hostname);
 
 					if (result.Status == IPStatus.Success) {
-						StoreResult(hostname, 1);
+						StoreResult(hostname, "1");
 						// Console.WriteLine("Ping succedded to {0} ({1}), round trip time = {2} ms. [tid = {3}]", hostname, result.Address.ToString(), result.RoundtripTime, tid);
 					} else {
-						StoreResult(hostname, 0);	
+						StoreResult(hostname, "0");	
 						// Console.WriteLine("Failed to ping host {0} (tid={1})", hostname, tid);
 					}
 				} catch {
-					StoreResult(hostname, 0);
+					StoreResult(hostname, "Could not ping");
 					// Console.WriteLine("Failed to ping host {0} (tid={1})", hostname, tid);
 				}
 			}
 		}
-		
-		public override void PrintStatus () {
-			while (HostQueue.Count > 0) {
-				Console.Write("Currently running {0} threads, {1} queued hostnames, {2} tested hostnames.\r", ThreadPoolDepth.ToString(), HostQueue.Count.ToString(), ResultQueue.Count.ToString());
-				Thread.Sleep(1000);
-			}
-			Console.WriteLine("Currently running {0} threads, {1} queued hostnames, {2} tested hostnames.", ThreadPoolDepth.ToString(), HostQueue.Count.ToString(), ResultQueue.Count.ToString());
-		}
 
-		public override void PrintResults() {
-			foreach (KeyValuePair<String, int> kvp in ResultQueue) {
-				Console.WriteLine("Ping status = {1} for host {0}.", kvp.Key, (kvp.Value != 0) ? "SUCCESS" : "Failure");
-			}
-		}
-		
 		public override void SaveResults() {
 		}
 	}
@@ -256,22 +294,6 @@ select distinct(i.[Host Name] + '.' + i.[Primary DNS Suffix]) -- r._ResourceGuid
 					StoreResult(hostname, sc.Status.ToString());
 				} catch {
 					StoreResult(hostname, "EXCEPTION");
-				}
-			}
-		}
-
-		public override void PrintStatus () {
-			while (HostQueue.Count > 0) {
-				Console.Write("Currently running {0} threads, {1} queued hostnames, {2} tested hostnames.\r", ThreadPoolDepth.ToString(), HostQueue.Count.ToString(), ResultQueue.Count.ToString());
-				Thread.Sleep(1000);
-			}
-			Console.WriteLine("Currently running {0} threads, {1} queued hostnames, {2} tested hostnames.", ThreadPoolDepth.ToString(), HostQueue.Count.ToString(), ResultQueue.Count.ToString());
-		}
-
-		public override void PrintResults() {
-			foreach (KeyValuePair<string, string> kvp in ResultQueue) {
-				if (kvp.Value != "EXCEPTION") {
-					Console.WriteLine("Altiris Agent service status for host {0}: {1}", kvp.Key, kvp.Value);
 				}
 			}
 		}
