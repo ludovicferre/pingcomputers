eea0a1d22a6749dc596a64e26ca6dc3ae178ea51
ludovicferre <ludovic.ferre@gmail.com>
2015-05-18 12:28:44 +0200

    Simplify service sources: action, status and excpt.

--- ServiceChecker.cs
+++ ServiceChecker.cs
@@ -22,25 +22,27 @@ namespace Symantec.CWoC {
 					testresult.host_guid = hostdata.host_guid;
 					ServiceController sc = new ServiceController(service_name, hostdata.host_name);
 					if (sc.Status == ServiceControllerStatus.Stopped) {
-						StoreResult(testresult, "service_status_stopped");
+						testresult.status = sc.Status.ToString();
+						StoreResult(testresult, "service_status");
 						int i = 0;
 						while (i < 5) {
 							sc.Start();
 							i++;
 							sc.Refresh();
-							if (sc.Status == ServiceControllerStatus.Running)
+							if (sc.Status == ServiceControllerStatus.Running) {
+								testresult.status = sc.Status.ToString();
+								StoreResult(testresult, "service_action");
 								break;
-							testresult.status = sc.Status.ToString();
-							StoreResult(testresult, "service_start");
+							}
 						}
 						if (i > 4) {
 							string status = String.Format("Failed to start the Altiris Agent service {0} times...", (i + 1).ToString());
 							testresult.status = status;
-							StoreResult(testresult, "service_start_failure");
+							StoreResult(testresult, "service_action");
 						}
 					} else {
 						testresult.status = sc.Status.ToString();
-						StoreResult(testresult, "service_status_started");
+						StoreResult(testresult, "service_status");
 					}
 				} catch (Exception e) {
 					testresult.status = e.Message;
